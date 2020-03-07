499c07b902c8a8d29c7df04649d2e61f36f7c9df
akp <aungkyawphyoe@yomabank.com>
2020-03-07 09:07:42 +0630

    Initial Commit

---
+++ IMC-HR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IMC-HR
+//
+//  Created by admin on 07/03/2020.
+//  Copyright © 2020 akp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IMC-HRTests/IMC_HRTests.swift
@@ -0,0 +1,34 @@
+//
+//  IMC_HRTests.swift
+//  IMC-HRTests
+//
+//  Created by admin on 07/03/2020.
+//  Copyright © 2020 akp. All rights reserved.
+//
+
+import XCTest
+@testable import IMC_HR
+
+class IMC_HRTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
